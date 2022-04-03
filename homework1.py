ee2ab07f483d8e248a50cd26b95843422171f253
narek-amaryan <narek.bf@gmail.com>
2022-04-03 22:05:58 +0400

    first commit

---
+++ homework1.py
@@ -0,0 +1,63 @@
+# BestComand = input('Your lovely Soccer team: ')
+# print('Hala', BestComand)
+
+#Easy
+#1
+# name = input('Your Name Please: ')
+# print('Hello', name)
+# surname = input('Please input your Surname: ')
+# print('Okay ',name, surname)
+# age = input('Please inpunt your age: ')
+# print('I think you are genius', name, surname)
+
+
+#2
+# money = input('Please say how much money do you have and i give you 40 more: ')
+# print('Check it',2 + int(money))
+
+#3
+# minimum_age = 18
+# age = int(input('Your age please:' ))
+#11
+# if age >= minimum_age:
+#     print('You can see this content')
+# else:
+#     print('This content unvailable for you')
+
+
+
+
+#normal 1
+# number = int(input('enter number higher than 0 and less than 10: '))
+# while number > 10 or number < 0:
+#     print('please enter again')
+#     number = int(input('please enter again a number higher than 0 and less than 10: '))
+# else:
+#     number = number ** 2
+#     print('thank you', number)
+
+
+
+#normal 2
+# number1 = int(input('Please enter the number for Messy ' ))
+# number2 = int(input('Please enter the number for Ronaldu ' ))
+# number1 = number1 + number2
+# number2 = number1 - number2
+# number1 = number1 - number2
+# print('The number for Messy is',number1,'The number for Ronaldu is',number2)
+
+
+#hard
+
+# name = input('Your name please: ')
+# surname = input('Your surname please:' )
+# age = int(input('Your age please: '))
+# weight = int(input('Your weight please: '))
+# if age <= 30 and weight > 50 and weight <= 120:
+#     print(name, surname, age,'year', 'weight', weight, 'You are good')
+# elif age >= 30 and weight <= 50 or weight >= 120:
+#     print(name, surname, age, 'year', 'weight', weight, 'You should keep yourself')
+# elif age >= 40 and weight <= 50 or weight >= 120:
+#     print(name, surname, age, 'year', 'weight', weight,'You need doctor')
+
+
